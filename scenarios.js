62c306f17f04fd5685e95b3c7b15d976f03352ad
link0257 <dshiell89@gmail.com>
2017-03-28 22:31:46 -0500

    new

---
+++ scenarios.js
@@ -0,0 +1,854 @@
+SCENARIO_DEFINITIONS =
+    [{
+        name: "#1 Black Barrow"
+        , decks:
+            ["Bandit Guard"
+            , "Bandit Archer"
+            , "Living Bones"]
+    },
+        {
+            name: "#2 Barrow Lair"
+        , decks:
+            ["Bandit Archer"
+            , "Boss"
+            , "Living Bones"
+            , "Living Corpse"
+            ]
+        },
+        {
+            name: "#3 Inox Encampment"
+        , decks:
+            ["Innox Guard"
+            , "Innox Archer"
+            , "Innox Shaman"]
+        },
+        {
+            name: "#4 Crypt of the Damned"
+        , decks:
+            ["Living Bones"
+            , "Bandit Archer"
+            , "Cultist"
+            , "Earth Demon"
+            , "Wind Demon"
+            ]
+        },
+        {
+            name: "#5 Ruinous Crypt"
+        , decks:
+            ["Cultist"
+            , "Living Bones"
+            , "Living Corpse"
+            , "Night Demon"
+            , "Flame Demon"
+            , "Frost Demon"
+            ]
+        },
+        {
+            name: "#6 Decaying Crypt"
+        , decks:
+            ["Living Bones"
+            , "Living Corpse"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#7 Vibrant Grotto"
+        , decks:
+            ["Forest Imp"
+            , "Cave Bear"
+            , "Innox Shaman"
+            , "Earth Demon"
+            ]
+        },
+        {
+            name: "#8 Gloomhaven Warehouse"
+        , decks:
+            ["Living Bones"
+            , "Living Corpse"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#9 Diamond Mine"
+        , decks:
+            ["Hound"
+            , "Vermling Scout"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#10 Plane of Elemental Power"
+        , decks:
+            ["Flame Demon"
+            , "Earth Demon"
+            , "Sun Demon"
+            ]
+        },
+        {
+            name: "#11 Gloomhaven Square A"
+        , decks:
+            ["Living Bones"
+            , "Living Corpse"
+            , "City Guard"
+            , "City Archer"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#12 Gloomhaven Square B"
+        , decks:
+            ["Living Bones"
+            , "Living Corpse"
+            , "Cultist"
+            , "City Guard"
+            , "City Archer"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#13 Temple of the Seer"
+        , decks:
+            ["Stone Golem"
+            , "Cave Bear"
+            , "Living Spirit"
+            , "Spitting Drake"
+            ]
+        },
+        {
+            name: "#14 Frozen Hollow"
+        , decks:
+            ["Hound"
+            , "Living Spirit"
+            , "Frost Demon"
+            ]
+        },
+        {
+            name: "#15 Shrine of Strength"
+        , decks:
+            ["Stone Golem"
+            , "Savvas Icestorm"
+            , "Frost Demon"
+            , "Wind Demon"
+            , "Harrower Infester"
+            ]
+        },
+        {
+            name: "#16 Mountain Pass"
+        , decks:
+            ["Earth Demon"
+            , "Wind Demon"
+            , "Innox Guard"
+            , "Innox Archer"
+            ]
+        },
+        {
+            name: "#17 Lost Island"
+        , decks:
+            ["Vermling Scout"
+            , "Vermling Shaman"
+            , "Cave Bear"
+            ]
+        },
+        {
+            name: "#18 Abandoned Sewers"
+        , decks:
+            ["Giant Viper"
+            , "Ooze"
+            , "Vermling Scout"
+            ]
+        },
+        {
+            name: "#19 Forgotten Crypt"
+        , decks:
+            ["Cultist"
+            , "Living Bones"
+            , "Living Spirit"
+            , "Living Corpse"
+            ]
+        },
+        {
+            name: "#20 Necromancer's Sanctum"
+        , decks:
+            ["Living Bones"
+            , "Cultist"
+            , "Night Demon"
+            , "Living Corpse"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#21 Infernal Throne"
+        , decks:
+            ["Sun Demon"
+            , "Frost Demon"
+            , "Night Demon"
+            , "Wind Demon"
+            , "Earth Demon"
+            , "Flame Demon"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#22 Temple of the Elements"
+        , decks:
+            ["Living Bones"
+            , "Cultist"
+            , "Earth Demon"
+            , "Flame Demon"
+            , "Frost Demon"
+            , "Wind Demon"
+            ]
+        },
+        {
+            name: "#23 Deep Ruins"
+        , decks:
+            ["Stone Golem"
+            , "Ancient Artillery"
+            , "Living Bones"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#24 Echo Chamber"
+        , decks:
+            ["Rending Drake"
+            , "Ooze"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#25 Icecrag Ascent"
+        , decks:
+            ["Hound"
+            , "Rending Drake"
+            , "Spitting Drake"
+            ]
+        },
+        {
+            name: "#26 Ancient Cistern"
+        , decks:
+            ["Living Corpse"
+            , "Ooze"
+            , "Night Demon"
+            , "Black Imp"
+            ]
+        },
+        {
+            name: "#27 Ruinous Rift"
+        , decks:
+            ["Night Demon"
+            , "Wind Demon"
+            , "Frost Demon"
+            , "Sun Demon"
+            , "Earth Demon"
+            , "Flame Demon"
+            ]
+        },
+        {
+            name: "#28 Outer Ritual Chamber"
+        , decks:
+            ["Living Corpse"
+            , "Cultist"
+            , "Living Bones"
+            , "Night Demon"
+            , "Sun Demon"
+            ]
+        },
+        {
+            name: "#29 Sanctuary of Gloom"
+        , decks:
+            ["Living Bones"
+            , "Living Corpse"
+            , "Living Spirit"
+            , "Black Imp"
+            ]
+        },
+        {
+            name: "#30 Shrine of the Depths"
+        , decks:
+            ["Ooze"
+            , "Lurker"
+            , "Deep Terror"
+            ]
+        },
+        {
+            name: "#31 Plane of the Night"
+        , decks:
+            ["Deep Terror"
+            , "Night Demon"
+            , "Black Imp"
+            ]
+        },
+        {
+            name: "#32 Decrepit Wood"
+        , decks:
+            ["Harrower Infester"
+            , "Giant Viper"
+            , "Deep Terror"
+            , "Black Imp"
+            ]
+        },
+        {
+            name: "#33 Savvas Armory"
+        , decks:
+            ["Savvas Icestorm"
+            , "Savvas Lavaflow"
+            , "Frost Demon"
+            , "Flame Demon"
+            , "Wind Demon"
+            , "Earth Demon"
+            ]
+        },
+        {
+            name: "#34 Scorched Summit"
+        , decks:
+            ["Rending Drake"
+            , "Spitting Drake"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#35 Gloomhaven Battlements A"
+        , decks:
+            ["Flame Demon"
+            , "Frost Demon"
+            , "Earth Demon"
+            , "Wind Demon"
+            , "City Guard"
+            , "City Archer"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#36 Gloomhaven Battlements B"
+        , decks:
+            ["Flame Demon"
+            , "Frost Demon"
+            , "Earth Demon"
+            , "Wind Demon"
+            , "City Archer"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#37 Doom Trench"
+        , decks:
+            ["Lurker"
+            , "Deep Terror"
+            , "Harrower Infester"
+            ]
+        },
+        {
+            name: "#38 Slave Pens"
+        , decks:
+            ["Innox Guard"
+            , "Innox Archer"
+            , "Innox Shaman"
+            , "Stone Golem"
+            ]
+        },
+        {
+            name: "#39 Treacherous Divide"
+        , decks:
+            ["Cave Bear"
+            , "Frost Demon"
+            , "Spitting Drake"
+            , "Cultist"
+            , "Living Bones"
+            ]
+        },
+        {
+            name: "#40 Ancient Defense Network"
+        , decks:
+            ["Living Corpse"
+            , "Flame Demon"
+            , "Cave Bear"
+            , "Stone Golem"
+            , "Forest Imp"
+            ]
+        },
+        {
+            name: "#41 Timeworn Tomb"
+        , decks:
+            ["Ancient Artillery"
+            , "Living Corpse"
+            , "Living Spirit"
+            , "Stone Golem"
+            ]
+        },
+        {
+            name: "#42 Realm of the Voice"
+        , decks:
+            ["Night Demon"
+            , "Wind Demon"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#43 - Drake Nest"
+        , decks:
+            ["Flame Demon"
+            , "Rending Drake"
+            , "Spitting Drake"
+            ]
+        },
+        {
+            name: "#44 Tribal Assault"
+        , decks:
+            ["Innox Guard"
+            , "Innox Archer"
+            , "Hound"
+            , "Innox Shaman"
+            ]
+        },
+        {
+            name: "#45 Rebel Swamp"
+        , decks:
+            ["City Guard"
+            , "City Archer"
+            , "Hound"
+            ]
+        },
+        {
+            name: "#46 Nightmare Peak"
+        , decks:
+            ["Night Demon"
+            , "Frost Demon"
+            , "Wind Demon"
+            , "Savvas Icestorm"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#47 Lair of the Unseeing Eye"
+        , decks:
+            ["Lurker"
+            , "Deep Terror"
+            , "Harrower Infester"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#48 - Shadow Weald"
+        , decks:
+            ["Forest Imp"
+            , "Earth Demon"
+            , "Harrower Infester"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#49 Rebel's Stand"
+        , decks:
+            ["Giant Viper"
+            , "City Archer"
+            , "City Guard"
+            , "Ancient Artillery"
+            ]
+        },
+        {
+            name: "#50 Ghost Fortress"
+        , decks:
+            ["Night Demon"
+            , "Sun Demon"
+            , "Earth Demon"
+            ]
+        },
+        {
+            name: "#51 The Void"
+        , decks:
+            ["Boss"
+            ]
+        },
+        {
+            name: "#52 Noxious Cellar"
+        , decks:
+            ["Spitting Drake"
+            , "Ooze"
+            , "Vermling Scout"
+            , "Living Corpse"
+            , "Vermling Shaman"
+            ]
+        },
+        {
+            name: "#53 Crypt Basement"
+        , decks:
+            ["Ooze"
+            , "Living Corpse"
+            , "Living Spirit"
+            , "Living Bones"
+            , "Giant Viper"
+            ]
+        },
+        {
+            name: "#54 Palace of Ice"
+        , decks:
+            ["Cave Bear"
+            , "Living Spirit"
+            , "Frost Demon"
+            , "Harrower Infester"
+            ]
+        },
+/*
+        { name: "#55 Foggy Thicket"
+        , decks:
+            [ "Random"
+            ]
+        },
+*/
+        {
+            name: "#56 Bandit's Wood"
+        , decks:
+            ["Hound"
+            , "Bandit Archer"
+            , "Rending Drake"
+            , "Bandit Guard"
+            ]
+        },
+        {
+            name: "#57 Investigation"
+        , decks:
+            ["City Guard"
+            , "City Archer"
+            , "Hound"
+            ]
+        },
+        {
+            name: "#58 Bloody Shack"
+        , decks:
+            ["Earth Demon"
+            , "Harrower Infester"
+            , "Black Imp"
+            , "City Guard"
+            ]
+        },
+        {
+            name: "#59 Forgotten Grove"
+        , decks:
+            ["Cave Bear"
+            , "Hound"
+            , "Forest Imp"
+            ]
+        },
+        {
+            name: "#60 Alchemy Lab"
+        , decks:
+            ["Ooze"
+            , "Giant Viper"
+            , "Hound"
+            , "Rending Drake"
+            , "Spitting Drake"
+            ]
+        },
+        {
+            name: "#61 Fading Lighthouse"
+        , decks:
+            ["Ooze"
+            , "Giant Viper"
+            , "Frost Demon"
+            , "Flame Demon"
+            ]
+        },
+        {
+            name: "#62 Pit of Souls"
+        , decks:
+            ["Living Bones"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#63 Magma Pit"
+        , decks:
+            ["Vermling Scout"
+            , "Innox Guard"
+            , "Innox Archer"
+            ]
+        },
+        {
+            name: "#64 Underwater Lagoon"
+        , decks:
+            ["Ooze"
+            , "Forest Imp"
+            , "Rending Drake"
+            ]
+        },
+        {
+            name: "#65 Sulfur Mine"
+        , decks:
+            ["Vermling Scout"
+            , "Hound"
+            , "Innox Shaman"
+            ]
+        },
+        {
+            name: "#66 Clockwork Cove"
+        , decks:
+            ["Ooze"
+            , "Ancient Artillery"
+            , "Living Spirit"
+            , "Stone Golem"
+            ]
+        },
+        {
+            name: "#67 Arcane Library"
+        , decks:
+            ["Forest Imp"
+            , "Cave Bear"
+            , "Stone Golem"
+            ]
+        },
+        {
+            name: "#68 Toxic Moor"
+        , decks:
+            ["Rending Drake"
+            , "Black Imp"
+            , "Giant Viper"
+            , "Living Corpse"
+            ]
+        },
+        {
+            name: "#69 Well of the Unfortunate"
+        , decks:
+            ["Vermling Scout"
+            , "Vermling Shaman"
+            , "Forest Imp"
+            , "Stone Golem"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#70 Chained Isle"
+        , decks:
+            ["Night Demon"
+            , "Wind Demon"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#71 Windswept Highlands"
+        , decks:
+            ["Spitting Drake"
+            , "Wind Demon"
+            , "Sun Demon"
+            ]
+        },
+        {
+            name: "#72 Oozing Grove"
+        , decks:
+            ["Ooze"
+            , "Forest Imp"
+            , "Giant Viper"
+            ]
+        },
+        {
+            name: "#73 Rockslide Ridge"
+        , decks:
+            ["Hound"
+            , "Innox Archer"
+            , "Ancient Artillery"
+            , "Innox Guard"
+            , "Innox Shaman"
+            ]
+        },
+        {
+            name: "#74 Merchant Ship"
+        , decks:
+            ["Bandit Guard"
+            , "Bandit Archer"
+            , "Lurker"
+            , "Deep Terror"
+            ]
+        },
+        {
+            name: "#75 Overgrown Graveyard"
+        , decks:
+            ["Living bones"
+            , "Living Corpse"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#76 Harrower Hive"
+        , decks:
+            ["Giant Viper"
+            , "Living Bones"
+            , "Night Demon"
+            , "Harrower Infester"
+            ]
+        },
+        {
+            name: "#77 Vault of Secrets"
+        , decks:
+            ["City Guard"
+            , "City Archer"
+            , "Golem"
+            , "Hound"
+            ]
+        },
+        {
+            name: "#78 Sacrifice Pit"
+        , decks:
+            ["Bandit Guard"
+            , "Bandit Archer"
+            , "Cultist"
+            , "Living Bones"
+            , "Black Imp"
+            ]
+        },
+        {
+            name: "#79 Lost Temple"
+        , decks:
+            ["Stone Golem"
+            , "Giant Viper"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#80 Vigil Keep"
+        , decks:
+            ["City Guard"
+            , "City Archer"
+            , "Ancient Artillery"
+            , "Hound"
+            ]
+        },
+        {
+            name: "#81 Temple of the Eclipse"
+        , decks:
+            ["Night Demon"
+            , "Sun Demon"
+            , "Stone Golem"
+            , "Ancient Artillery"
+            , "Boss"
+            ]
+        },
+        {
+            name: "#82 Burning Mountain"
+        , decks:
+            ["Earth Demon"
+            , "Fire Demon"
+            , "Stone Golem"
+            ]
+        },
+        {
+            name: "#83 Shadows Within"
+        , decks:
+            ["Hound"
+            , "Cultist"
+            , "Living Bones"
+            , "Living Spirit"
+            , "Flame Demon"
+            ]
+        },
+        {
+            name: "#84 Crystalline Cave"
+        , decks:
+            ["Flame Demon"
+            , "Frost Demon"
+            , "Earth Demon"
+            ]
+        },
+        {
+            name: "#85 Sun Temple"
+        , decks:
+            ["Hound"
+            , "Black Imp"
+            , "Night Demon"
+            , "Sun Demon"
+            ]
+        },
+        {
+            name: "#86 Harried Village"
+        , decks:
+            ["Cave Bear"
+            , "Vermling Shaman"
+            , "Vermling Scout"
+            , "Lurker"
+            ]
+        },
+        {
+            name: "#87 Corrupted Cove"
+        , decks:
+            ["Lurker"
+            , "Deep Terror"
+            , "Ooze"
+            , "Imp"
+            ]
+        },
+        {
+            name: "#88 Plane of Water"
+        , decks:
+            ["Frost Demon"
+            , "Ooze"
+            , "Lurker"
+            ]
+        },
+        {
+            name: "#89 Syndicate Hideout"
+        , decks:
+            ["Bandit Archer"
+            , "Bandit Guard"
+            , "Cultist"
+            , "Giant Viper"
+            ]
+        },
+        {
+            name: "#90 Demonic Rift"
+        , decks:
+            ["Earth Demon"
+            , "Wind Demon"
+            , "Night Demon"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#91 Wild Melee"
+        , decks:
+            ["Cave Bear"
+            , "Hound"
+            , "Bandit Guard"
+            , "Bandit Archer"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#92 Back Alley Brawl"
+        , decks:
+            ["Bandit Guard"
+            , "Bandit Archer"
+            , "Innox  Guard"
+            , "City Archer"
+            , "City  Guard"
+            , "Savvas Icestorm"
+            , "Frost Demon"
+            , "Wind Demon"
+            ]
+        },
+        {
+            name: "#93 Sunken Vessel"
+        , decks:
+            ["Lurker"
+            , "Frost Demon"
+            , "Living Spirit"
+            ]
+        },
+        {
+            name: "#94 Vermling Nest"
+        , decks:
+            ["Hound"
+            , "Vermling Scout"
+            , "Vermling Shaman"
+            , "Cave Bear"
+            ]
+        },
+        {
+            name: "#95 Payment Due"
+        , decks:
+            ["Deep Terror"
+            , "Flame Demon"
+            , "Earth Demon"
+            , "Savvas Lavaflow"
+            ]
+        },
+
+    ];
